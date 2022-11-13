14cf46a3213e3dfd1acec9850d0cfbfc434d5c37
nico <nico@aleph.engineering>
2022-11-13 13:40:44 -0500

    #1 Injected abstraction IPersonService will be replaced by concretion PersonService When you have more than one implementation of this abstraction, the injector will not know which one it has to automatically inject.

--- src/main/java/com/example/expcond_rest_api/controllers/PersonController.java
+++ src/main/java/com/example/expcond_rest_api/controllers/PersonController.java
@@ -1,7 +1,7 @@
 package com.example.expcond_rest_api.controllers;
 
 import com.example.expcond_rest_api.entities.Person;
-import com.example.expcond_rest_api.services.abstraction.IPersonService;
+import com.example.expcond_rest_api.services.PersonService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/persons")
 public class PersonController {
 
-    private final IPersonService personService;
+    private final PersonService personService;
 
     Pageable pageable = PageRequest.of(0, 5);
 
-    public PersonController(IPersonService personService) {
+    public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
