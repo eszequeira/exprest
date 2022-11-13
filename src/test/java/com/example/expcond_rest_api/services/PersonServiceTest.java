73451c73910f90f984ce1a796b952f8878edbcf8
nico <nico@aleph.engineering>
2022-11-13 14:34:35 -0500

    #1 Create PersonServiceTest

---
+++ src/test/java/com/example/expcond_rest_api/services/PersonServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.expcond_rest_api.services;
+
+import com.example.expcond_rest_api.persistence.factory.PersonEntityFactory;
+import com.example.expcond_rest_api.persistence.repositories.PersonRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PersonServiceTest {
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @BeforeAll
+    void insertTestData() {
+        var john = PersonEntityFactory.build(1L, "John", "Doe");
+        personRepository.save(john);
+
+        var jane = PersonEntityFactory.build(2L, "Jane", "Doe");
+        personRepository.save(jane);
+    }
+
+    @Test
+    public void givenMeasurementService_whenTemperatureEvent_thenIsCorrectlyProcessed() {
+        var person = personService.findPerson(1L);
+
+        assertThat(person.getNOMBRE()).isEqualTo("John");
+    }
+
+    @Test
+    public void givenMeasurementService_whenObjectPresentEvent_thenIsCorrectlyProcessed() {
+        var allItems = personRepository.findAll();
+
+        assertThat(allItems.size()).isEqualTo(2);
+    }
+
+}
