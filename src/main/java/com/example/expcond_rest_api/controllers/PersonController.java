3d2d75481ad22d1cf58ac220843778e3339f34b2
nico <nico@aleph.engineering>
2022-11-13 13:39:12 -0500

    #1 Autowired annotation will be replaced by constructor injection

--- src/main/java/com/example/expcond_rest_api/controllers/PersonController.java
+++ src/main/java/com/example/expcond_rest_api/controllers/PersonController.java
@@ -2,19 +2,25 @@ package com.example.expcond_rest_api.controllers;
 
 import com.example.expcond_rest_api.entities.Person;
 import com.example.expcond_rest_api.services.abstraction.IPersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/persons")
 public class PersonController {
 
-    @Autowired
-    private IPersonService personService;
+    private final IPersonService personService;
+
     Pageable pageable = PageRequest.of(0, 5);
+
+    public PersonController(IPersonService personService) {
+        this.personService = personService;
+    }
 
     @GetMapping
     public Page<Person> getPersons() {
